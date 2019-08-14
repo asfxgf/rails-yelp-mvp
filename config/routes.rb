e8a61700516fbc22dea5bc2e68f8f3092c0a7088
quentin deleglise <deleglise.quentin@hotmail.fr>
2019-08-14 15:11:31 +0200

    models done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # a user can see all restaurants
+  get "restaurants", to: "restaurants#index"
+  # a user can add a new restaurant and must return show
+  get "restaurants/new", to: "restaurants#new"
+  # a user can show a restaurant
+  get "restaurants/:id", to: "restaurants#show", as: :show
+
+  post "restaurants", to: "restaurants#create"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'destroying restaurants ...'
+
+Restaurant.destroy_all
+
+puts 'reloading restaurants ...'
+restaurants_attributes = [
+  {
+    name: 'mcdo',
+    address: '14 rue petit',
+    phone_number: '0777258634',
+    category: 'chinese'
+  },
+  {
+    name: 'buffalo',
+    address: '17 rue lor',
+    phone_number: '0777147686',
+    category: 'chinese'
+  },
+  {
+    name: 'pitaya',
+    address: '25 rue oberk',
+    phone_number: '0777217456',
+    category: 'chinese'
+  },
+  {
+    name: 'five',
+    address: '14 rue du gras',
+    phone_number: '0777317142',
+    category: 'chinese'
+  },
+  {
+    name: 'le refuge des fondus',
+    address: '28 rue montmartre',
+    phone_number: '0712417196',
+    category: 'chinese'
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
